5c63ff64dbbbf0e0612849793e71bbd270444ce6
Stepami <Stepami@mail.ru>
2025-03-04 13:17:29 +0300

    minor fix

--- HdbscanSharp/Utils/BitSet.cs
+++ HdbscanSharp/Utils/BitSet.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 
-namespace HdbscanSharp.Utils
+namespace HdbscanSharp.Utils;
+
+public class BitSet
 {
-	public class BitSet
+	private bool[] _bits = [];
+		
+	public bool Get(int pos)
 	{
-		private bool[] _bits = new bool[0];
+		return pos < _bits.Length && _bits[pos];
+	}
 		
-		public bool Get(int pos)
-		{
-			return pos < _bits.Length && _bits[pos];
-		}
+	public void Set(int pos)
+	{
+		Ensure(pos);
+		_bits[pos] = true;
+	}
 		
-		public void Set(int pos)
+	private void Ensure(int pos)
+	{
+		if (pos >= _bits.Length)
 		{
-			Ensure(pos);
-			_bits[pos] = true;
-		}
-		
-		private void Ensure(int pos)
-		{
-			if (pos >= _bits.Length)
-			{
-				var nd = new bool[pos + 64];
-				Array.Copy(_bits, 0, nd, 0, _bits.Length);
-				_bits = nd;
-			}
+			var nd = new bool[pos + 64];
+			Array.Copy(_bits, 0, nd, 0, _bits.Length);
+			_bits = nd;
 		}
 	}
 }
